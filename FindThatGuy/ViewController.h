9ef68991e18b71448cc55c455ec5d1f9b88eb9c8
Brentton Garber <btgarber@Brenttons-MacBook-Pro.local>
2014-04-18 14:39:46 -0700

    Initial Commit

---
+++ FindThatGuy/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  FindThatGuy
+//
+//  Created by Brentton Garber on 4/18/14.
+//  Copyright (c) 2014 ASU. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
